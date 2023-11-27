5dd11114d6ed13df9611dfed6aad5e466b8aeef9
Matsuel <matsuel0308@gmail.com>
2023-11-27 16:44:44 +0100

    init project and navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-wrapper'>
+        <div className="navbar-logo">
+AAAAAAAAAA
+        </div>
+        <div className="navbar-links">
+            <div className="link">
+                <a href="/" className='a-link'><span className='num-link'>01.</span> Home</a>
+            </div>
+            <div className="link">
+                <a href="/" className='a-link'><span className='num-link'>01.</span> Home</a>
+            </div>
+            <div className="link">
+                <a href="/" className='a-link'><span className='num-link'>01.</span> Home</a>
+            </div>
+            <div className="link">
+                <a href="/" className='a-link'><span className='num-link'>01.</span> Home</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
