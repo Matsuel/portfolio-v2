72216d706fb7a66e2de12b4e11175133440095f3
Mathéo Lang <matsuel0308@gmail.com>
2023-11-28 21:39:29 +0100

    add active links border

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+    const [linkSelected, setLinkSelected] = useState(-1);
+
+    const handleLinkClick = (index:number) => {
+        if (linkSelected === index) {
+          setLinkSelected(-1);
+        } else {
+          setLinkSelected(index);
+        }
+      };
+
+    const linksHref=["#home","#about","#projects","#contact"]
+    const linksName=["Home","About","Projects","Contact"]
+
   return (
     <div className='navbar-wrapper'>
         <div className="navbar-logo">
-AAAAAAAAAA
         </div>
         <div className="navbar-links">
-            <div className="link">
-                <a href="/" className='a-link'><span className='num-link'>01.</span> Home</a>
-            </div>
-            <div className="link">
-                <a href="/" className='a-link'><span className='num-link'>01.</span> Home</a>
-            </div>
-            <div className="link">
-                <a href="/" className='a-link'><span className='num-link'>01.</span> Home</a>
-            </div>
-            <div className="link">
-                <a href="/" className='a-link'><span className='num-link'>01.</span> Home</a>
-            </div>
+            {[0,1,2,3].map((index)=>(
+                <div className="link" key={index}>
+                    <a href={linksHref[index]}
+                    className={`a-link ${linkSelected === index ? 'active-link' : ''}`}
+                    onClick={() => handleLinkClick(index)}
+                    >
+                        <span className='num-link'>0{index+1}.</span> {linksName[index]}
+                    </a>
+                </div>
+            ))}
         </div>
     </div>
   )
