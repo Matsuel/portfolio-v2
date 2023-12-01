8887e9f0f9fc58da4e1f5e750f11dc7a5a8c7bc2
Mathéo Lang <matsuel0308@gmail.com>
2023-12-01 19:58:16 +0100

    skills sections with style

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -7,10 +7,15 @@ import SkillsDataProvider from './SkillsDataProvider'
 const Skills = () => {
     return (
         <section
-            id="skills"
+            id="about"
             className="section-skills"
             style={{ transform: "scale(0.9)" }}
         >
+            <div className="text-skills">
+                <h2 className="section-title">Skills 🛠️</h2>
+                <p className="section-subtitle">Currently a student, I am immersed in the captivating world of IT and development, I am training in a variety of programming languages ​​which allow me to acquire skills and allow me to become more efficient in my different projects</p>
+            </div>
+
             <div className="skills-wrap">
                 {Skill_data.map((image, index) => (
                     <SkillsDataProvider
