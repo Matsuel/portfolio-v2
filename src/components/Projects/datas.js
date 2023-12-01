89e52249eb6ac62bf7d253711a2e4cb2d811052a
Mathéo Lang <matsuel0308@gmail.com>
2023-12-01 22:52:06 +0100

    add datas projects

---
+++ src/components/Projects/datas.js
@@ -0,0 +1,52 @@
+export const ProjectsDatas = [
+    {
+        title: "Portfolio",
+        githubLink: "https://github.com/Matsuel/portfolio",
+        liveLink: "ertyui",
+        technologies: ["React - ", "Css - ", "Framer Motion"],
+        description:"First website made with css"
+    },
+    {
+        title:"Whastapp Clone",
+        githubLink:"https://github.com/Matsuel/Whastapp-Clone",
+        liveLink:"",
+        technologies:["React - ","Sqlite - ", "Css - ", "WebSockets"],
+        description:"School project"
+    },
+    {
+        title:"Weather App",
+        githubLink:"https://github.com/Matsuel/AppMeteo-ReactNative",
+        liveLink:"",
+        technologies:["React Native - ", "Css"],
+        description:"First mobile app made with React Native"
+    },
+    {
+        title:"LeRobot",
+        githubLink:"https://github.com/Matsuel/LeRobot",
+        liveLink:"",
+        technologies:["JavasScript - ", "NodeJs"],
+        description:"Discord bot, he can generate memes, get cryptos prices, discord stats, etc..."
+    },
+    {
+        title:"CalculatorQuest",
+        githubLink:"https://github.com/Matsuel/CalculatorQuest",
+        liveLink:"",
+        technologies:["C - ", "Avalonia"],
+        description:"Calculator made with Avalonia for final school project"
+    },
+    {
+        title:"Taquin",
+        githubLink:"https://github.com/Matsuel/Taquin-Python",
+        liveLink:"",
+        technologies:["Python"],
+        description:"Taquin game made with python build at lycee"
+    },
+    {
+        title:"Weather App",
+        githubLink:"",
+        liveLink:"",
+        technologies:["C - ", "Avalonia"],
+        description:"Weather app made with Avalonia for final school project"
+    }
+];
+        
