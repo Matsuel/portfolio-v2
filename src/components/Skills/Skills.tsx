eaf76048cddd8aa22fce025c4ae8f4a6d666e369
Mathéo Lang <matsuel0308@gmail.com>
2023-12-15 16:13:20 +0100

    add parallax and format doc

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import './Skills.css'
 import { Skill_data } from './datas'
 import SkillsDataProvider from './SkillsDataProvider'
+import { useParallax } from 'react-scroll-parallax';
 
 
 const Skills = () => {
+
+    const parallaxTitle = useParallax<HTMLDivElement>({rotate: [180, -70], translateX: [-100, 100]});
+    const parallaxSubtitle = useParallax<HTMLDivElement>({translateX: [-100, 100]});
     return (
         <section
             id="about"
@@ -12,8 +16,8 @@ const Skills = () => {
             style={{ transform: "scale(0.9)" }}
         >
             <div className="text-skills">
-                <h2 className="section-title">Skills 🛠️</h2>
-                <p className="section-subtitle">Currently a student, I am immersed in the captivating world of IT and development, I am training in a variety of programming languages ​​which allow me to acquire skills and allow me to become more efficient in my different projects</p>
+                <h2 ref={parallaxTitle.ref} className="section-title">Skills 🛠️</h2>
+                <p ref={parallaxSubtitle.ref} className="section-subtitle">Currently a student, I am immersed in the captivating world of IT and development, I am training in a variety of programming languages ​​which allow me to acquire skills and allow me to become more efficient in my different projects</p>
             </div>
 
             <div className="skills-wrap">
--- src/components/Skills/SkillsDataProvider.tsx
+++ src/components/Skills/SkillsDataProvider.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {motion} from 'framer-motion'
-import {useInView}  from 'react-intersection-observer'
+import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 import Bootstrap from '../../assets/skills/bootstrap.svg'
 import Cpp from '../../assets/skills/c++.svg'
 import Css from '../../assets/skills/css.svg'
@@ -15,68 +15,55 @@ import Reactjs from '../../assets/skills/react.svg'
 import ReactNative from '../../assets/skills/react-native.svg'
 import Typescript from '../../assets/skills/typescript.svg'
 import TailwindCss from '../../assets/skills/tailwindcss.svg'
-
-
-
+import { useParallax } from 'react-scroll-parallax';
 
 interface Props {
     src: string,
     index: number,
 }
 
-// const GetImage = (src:string) => {
-//     switch (src) {
-//         case 'css':
-//             return Css
-//         default:
-//             return Css
-//     }
-// }
-
-const SkillsDataProvider = ({src,index}:Props) => {
-
-    const windowHeight = window.innerHeight*0.15
+const SkillsDataProvider = ({ src, index }: Props) => {
 
     const [ref, inView] = useInView({
         triggerOnce: true,
     })
 
+    const windowHeight = window.innerHeight * 0.15
+
     const imageVariants = {
-        hidden:{opacity:0},
-        visible:{opacity:1}
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 }
     }
 
     const animationDelay = 0.3
-
-  return (
-    <motion.div
-    ref={ref}
-    initial="hidden"
-    variants={imageVariants}
-    animate={inView ? "visible" : "hidden"}
-    custom={index}
-    transition={{delay:animationDelay*index}}
-    >
-        {
-            src=== 'bootstrap' ? <img src={Bootstrap} height={windowHeight} alt="Bootstrap" /> :
-            src=== 'cpp' ? <img src={Cpp} height={windowHeight} alt="C++" /> :
-            src=== 'css' ? <img src={Css} height={windowHeight} alt="CSS" /> :
-            src=== 'golang' ? <img src={Golang} height={windowHeight} alt="Golang" /> :
-            src=== 'html' ? <img src={Html} height={windowHeight} alt="HTML" /> :
-            src=== 'java' ? <img src={Java} height={windowHeight} alt="Java" /> :
-            src=== 'javascript' ? <img src={Javascript} height={windowHeight} alt="Javascript" /> :
-            src=== 'php' ? <img src={Php} height={windowHeight} alt="PHP" /> :
-            src=== 'python' ? <img src={Python} height={windowHeight} alt="Python" /> :
-            src=== 'react' ? <img src={Reactjs} height={windowHeight} alt="React" /> :
-            src=== 'react-native' ? <img src={ReactNative} height={windowHeight} alt="React Native" /> :
-            src=== 'typescript' ? <img src={Typescript} height={windowHeight} alt="Typescript" /> :
-            src=== 'tailwindcss' ? <img src={TailwindCss} height={windowHeight} alt="Tailwindcss" /> :
-            src=== 'csharp' ? <img src={Csharp} height={windowHeight} alt="C#" /> :
-            <img src={Css} height={windowHeight} alt="CSS" />
-        }
-
-    </motion.div>
-  )
+    return (
+        <motion.div
+            ref={ref}
+            initial="hidden"
+            variants={imageVariants}
+            animate={inView ? "visible" : "hidden"}
+            custom={index}
+            transition={{ delay: animationDelay * index }}
+        >
+            {
+                src === 'bootstrap' ? <img src={Bootstrap} height={windowHeight} alt="Bootstrap" /> :
+                    src === 'cpp' ? <img src={Cpp} height={windowHeight} alt="C++" /> :
+                        src === 'css' ? <img src={Css} height={windowHeight} alt="CSS" /> :
+                            src === 'golang' ? <img src={Golang} height={windowHeight} alt="Golang" /> :
+                                src === 'html' ? <img src={Html} height={windowHeight} alt="HTML" /> :
+                                    src === 'java' ? <img src={Java} height={windowHeight} alt="Java" /> :
+                                        src === 'javascript' ? <img src={Javascript} height={windowHeight} alt="Javascript" /> :
+                                            src === 'php' ? <img src={Php} height={windowHeight} alt="PHP" /> :
+                                                src === 'python' ? <img src={Python} height={windowHeight} alt="Python" /> :
+                                                    src === 'react' ? <img src={Reactjs} height={windowHeight} alt="React" /> :
+                                                        src === 'react-native' ? <img src={ReactNative} height={windowHeight} alt="React Native" /> :
+                                                            src === 'typescript' ? <img src={Typescript} height={windowHeight} alt="Typescript" /> :
+                                                                src === 'tailwindcss' ? <img src={TailwindCss} height={windowHeight} alt="Tailwindcss" /> :
+                                                                    src === 'csharp' ? <img src={Csharp} height={windowHeight} alt="C#" /> :
+                                                                        <img src={Css} height={windowHeight} alt="CSS" />
+            }
+        </motion.div>
+    )
 }
 
 export default SkillsDataProvider
